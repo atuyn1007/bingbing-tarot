db5673ac073e898924e5baf32420ccd5f981c8ec
atuyn1007 <atuyn1007@gmail.com>
2026-06-12 18:56:54 +0800

    Fix Supabase auth lock contention by removing StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,8 +5,6 @@ import { preloadInitialLanguage } from './i18n'
 
 preloadInitialLanguage().finally(() => {
   createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
+    <App />,
   )
 })
